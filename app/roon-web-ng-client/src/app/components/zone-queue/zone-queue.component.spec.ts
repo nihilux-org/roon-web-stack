491b8f9a14c174301412184b4313629e9fb0d1af
nihil <nihil@nihilux.org>
2024-02-28 13:06:14 -0500

    bugfix: Live Radio info are not displayed when a Live Radio is playing

--- app/roon-web-ng-client/src/app/components/zone-queue/zone-queue.component.spec.ts
+++ app/roon-web-ng-client/src/app/components/zone-queue/zone-queue.component.spec.ts
@@ -2,7 +2,7 @@ import { MockBuilder, MockedComponentFixture, MockedDebugElement, MockRender, ng
 import { computed, Signal, signal, WritableSignal } from "@angular/core";
 import { BrowserAnimationsModule, NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { QueueState } from "@model";
-import { EMPTY_QUEUE_TRACK, EMPTY_TRACK, TrackDisplay } from "@model/client";
+import { EMPTY_TRACK, TrackDisplay } from "@model/client";
 import { RoonService } from "@services/roon.service";
 import { SettingsService } from "@services/settings.service";
 import { ZoneQueueComponent } from "./zone-queue.component";
