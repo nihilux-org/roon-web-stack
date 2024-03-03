b87b89a1d07480966cced983913575f707c9d65a
nihil <nihil@nihilux.org>
2024-03-03 08:00:16 -0500

    fix: standardize logger config (#28)

--- app/roon-web-api/src/infrastructure/logger.ts
+++ app/roon-web-api/src/infrastructure/logger.ts
@@ -1,11 +1,35 @@
-import { pino } from "pino";
+import { LoggerOptions, pino } from "pino";
 import * as process from "process";
 
-export const logger = pino({
-  level: process.env["LOG_LEVEL"] ?? "info",
-  formatters: {
-    bindings: (bindings) => ({ hostname: bindings.hostname as string }),
-    level: (label) => ({ level: label.toUpperCase() }),
-  },
-  timestamp: pino.stdTimeFunctions.isoTime,
-});
+export const buildLoggerOptions: (minLevel?: pino.Level) => LoggerOptions | undefined = (minLevel?: pino.Level) => {
+  const level = process.env["LOG_LEVEL"] ?? "info";
+  let shouldConfigureLogger = true;
+  if (minLevel) {
+    switch (minLevel) {
+      case "debug":
+        shouldConfigureLogger = level === "debug" || level === "trace";
+        break;
+      case "info":
+        shouldConfigureLogger = level === "debug" || level === "trace" || level === "info";
+        break;
+      case "trace":
+        shouldConfigureLogger = level === "trace";
+        break;
+      case "warn":
+        shouldConfigureLogger = level === "debug" || level === "trace" || level === "info" || level === "warn";
+        break;
+    }
+  }
+  if (shouldConfigureLogger) {
+    return {
+      level,
+      formatters: {
+        bindings: (bindings) => ({ hostname: bindings.hostname as string }),
+        level: (label) => ({ level: label.toUpperCase() }),
+      },
+      timestamp: pino.stdTimeFunctions.isoTime,
+    };
+  }
+};
+
+export const logger = pino(buildLoggerOptions());
