59ad56a5165c905885a31551b96f3b2e62730581
nihil <nihil@nihilux.org>
2024-03-07 16:29:55 -0500

    feat: use dialog for volume-drawer to enhance accessibility and fix keyboard navigation (#44)

--- app/roon-web-ng-client/src/app/components/zone-volume/zone-volume.component.spec.ts
+++ app/roon-web-ng-client/src/app/components/zone-volume/zone-volume.component.spec.ts
@@ -1,26 +1,19 @@
 import { MockBuilder, MockedComponentFixture, MockRender } from "ng-mocks";
 import { signal, WritableSignal } from "@angular/core";
-import { Command, CommandType, VolumeCommand } from "@model";
 import { ZoneCommands, ZoneCommandState } from "@model/client";
-import { RoonService } from "@services/roon.service";
 import { SettingsService } from "@services/settings.service";
 import { ZoneVolumeComponent } from "./zone-volume.component";
 
 describe("ZoneVolumeComponent", () => {
   let component: ZoneVolumeComponent;
   let fixture: MockedComponentFixture<ZoneVolumeComponent, { zoneCommands: ZoneCommands }>;
-  let commands: VolumeCommand[];
   let zoneCommands: ZoneCommands;
-  let roonService: {
-    volume: jest.Mock;
-  };
   let $isSmallScreen: WritableSignal<boolean>;
   let settingsService: {
     isSmallScreen: jest.Mock;
   };
 
   beforeEach(async () => {
-    commands = [];
     zoneCommands = {
       zoneId: "zone_id",
       previousTrack: ZoneCommandState.ABSENT,
@@ -30,20 +23,11 @@ describe("ZoneVolumeComponent", () => {
       nextTrack: ZoneCommandState.ABSENT,
       outputs: [],
     };
-    roonService = {
-      volume: jest.fn().mockImplementation((command: Command) => {
-        if (command.type === CommandType.VOLUME) {
-          commands.push(command);
-        }
-      }),
-    };
     $isSmallScreen = signal(false);
     settingsService = {
       isSmallScreen: jest.fn().mockImplementation(() => $isSmallScreen),
     };
-    await MockBuilder(ZoneVolumeComponent)
-      .mock(RoonService, roonService as Partial<RoonService>)
-      .mock(SettingsService, settingsService as Partial<SettingsService>);
+    await MockBuilder(ZoneVolumeComponent).mock(SettingsService, settingsService as Partial<SettingsService>);
     fixture = MockRender(ZoneVolumeComponent, {
       zoneCommands,
     });
