6b5bf04b5c06409a8b313588f0b306dfbd7df076
nihil <nihil@nihilux.org>
2024-04-13 20:00:41 -0400

    chore: prepare v0.0.7 release (#124)

--- app/roon-web-api/src/infrastructure/roon-extension.ts
+++ app/roon-web-api/src/infrastructure/roon-extension.ts
@@ -16,7 +16,7 @@ import {
 } from "@model";
 import { Extension } from "@roon-kit";
 
-export const extension_version = "0.0.7-beta-15";
+export const extension_version = "0.0.7";
 
 const extension: RoonExtension = new Extension({
   description: {
