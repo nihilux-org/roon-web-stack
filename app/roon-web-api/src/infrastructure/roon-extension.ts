21fe648e7cc75eb8ef26b71302894d493a8056f1
nihil <nihil@nihilux.org>
2024-04-19 18:17:00 -0400

    feat: add fullscreen support

--- app/roon-web-api/src/infrastructure/roon-extension.ts
+++ app/roon-web-api/src/infrastructure/roon-extension.ts
@@ -16,7 +16,7 @@ import {
 } from "@model";
 import { Extension } from "@roon-kit";
 
-export const extension_version = "0.0.8-beta-2";
+export const extension_version = "0.0.8-beta-3";
 
 const extension: RoonExtension = new Extension({
   description: {
