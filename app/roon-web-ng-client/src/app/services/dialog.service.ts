fe51e8d0b96b0c4dae9b94f61434bd9bad444a97
nihil <nihil@nihilux.org>
2024-09-02 12:49:59 -0400

    chore: add idle-service

--- app/roon-web-ng-client/src/app/services/dialog.service.ts
+++ app/roon-web-ng-client/src/app/services/dialog.service.ts
@@ -1,6 +1,7 @@
 import { ComponentType } from "@angular/cdk/overlay";
 import { Injectable, Signal } from "@angular/core";
 import { MatDialog, MatDialogConfig, MatDialogRef } from "@angular/material/dialog";
+import { IdleService } from "@services/idle.service";
 import { SettingsService } from "@services/settings.service";
 
 @Injectable({
@@ -8,23 +9,36 @@ import { SettingsService } from "@services/settings.service";
 })
 export class DialogService {
   private readonly _dialog: MatDialog;
+  private readonly _idleService: IdleService;
   private readonly _$layoutClass: Signal<string>;
   private _openedDialog?: MatDialogRef<unknown>;
+  private _isIdleWatched: boolean;
 
-  constructor(dialog: MatDialog, settingsService: SettingsService) {
+  constructor(dialog: MatDialog, idleService: IdleService, settingsService: SettingsService) {
     this._dialog = dialog;
+    this._idleService = idleService;
     this._$layoutClass = settingsService.displayModeClass();
+    this._isIdleWatched = false;
   }
 
   open<T, D = never>(component: ComponentType<T>, config?: MatDialogConfig<D>): void {
     const panelClass = [...((config?.panelClass ?? []) as string[])];
     panelClass.push("nr-dialog-custom", this._$layoutClass());
+    this._isIdleWatched = this._idleService.isWatching();
     this._openedDialog?.close();
     this._openedDialog = this._dialog.open(component, {
       ...config,
       panelClass,
     });
-    this._openedDialog.afterClosed().subscribe(() => {
+    if (this._isIdleWatched) {
+      this._openedDialog.afterOpened().subscribe(() => {
+        this._idleService.stopWatch();
+      });
+    }
+    this._openedDialog.beforeClosed().subscribe(() => {
+      if (this._isIdleWatched) {
+        this._idleService.startWatch();
+      }
       delete this._openedDialog;
     });
   }
