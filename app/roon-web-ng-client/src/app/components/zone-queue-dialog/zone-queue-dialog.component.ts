38dfdb11287c9eea1a939f89718504cd6f338ffc
nihil <nihil@nihilux.org>
2024-11-24 19:24:26 -0500

    fix: focus in queue dialog during spatial-navigation (#379)

--- app/roon-web-ng-client/src/app/components/zone-queue-dialog/zone-queue-dialog.component.ts
+++ app/roon-web-ng-client/src/app/components/zone-queue-dialog/zone-queue-dialog.component.ts
@@ -1,10 +1,10 @@
 import { Subscription } from "rxjs";
 import { NgTemplateOutlet } from "@angular/common";
-import { ChangeDetectionStrategy, Component, inject, OnDestroy, Signal, TemplateRef } from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, TemplateRef } from "@angular/core";
 import { MatButton } from "@angular/material/button";
 import { MAT_DIALOG_DATA, MatDialogActions, MatDialogContent, MatDialogRef } from "@angular/material/dialog";
 import { MatIcon } from "@angular/material/icon";
-import { LayoutContext, TrackDisplay } from "@model/client";
+import { LayoutContext } from "@model/client";
 import { SettingsService } from "@services/settings.service";
 
 @Component({
@@ -18,18 +18,15 @@ export class ZoneQueueDialogComponent implements OnDestroy {
   private readonly _dialogRef: MatDialogRef<ZoneQueueDialogComponent>;
   private readonly _dialogRefCloseSubscription: Subscription;
   private readonly _settingsService: SettingsService;
-  readonly $trackDisplay: Signal<TrackDisplay>;
   readonly queueComponentTemplateRef: TemplateRef<LayoutContext>;
   readonly queueComponentTemplateContext: LayoutContext;
 
   constructor() {
     const data = inject(MAT_DIALOG_DATA) as {
-      $trackDisplay: Signal<TrackDisplay>;
       queueComponentTemplateRef: TemplateRef<LayoutContext>;
     };
     this._dialogRef = inject<MatDialogRef<ZoneQueueDialogComponent>>(MatDialogRef);
     this._settingsService = inject(SettingsService);
-    this.$trackDisplay = data.$trackDisplay;
     this.queueComponentTemplateRef = data.queueComponentTemplateRef;
     this.queueComponentTemplateContext = {
       class: `in-dialog ${this._settingsService.displayModeClass()()}`,
