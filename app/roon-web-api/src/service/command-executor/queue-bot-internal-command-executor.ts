bfcdbb1ca7e9b16733e4a5f3aaa99e9ea7581a69
nihil <nihil@nihilux.org>
2025-02-05 13:44:53 -0500

    fix: ensure queue bot skips dummy track (#454)

--- app/roon-web-api/src/service/command-executor/queue-bot-internal-command-executor.ts
+++ app/roon-web-api/src/service/command-executor/queue-bot-internal-command-executor.ts
@@ -11,11 +11,9 @@ import { awaitAll } from "./command-executor-utils";
 export const internalExecutor: InternalCommandExecutor<QueueBotCommand, FoundZone> = async (command, foundZone) => {
   const { server, zone } = foundZone;
   if (zone.state === "playing") {
-    await server.services.RoonApiTransport.control(zone, zone.is_next_allowed ? "pause" : "stop");
-    if (zone.is_next_allowed) {
-      await server.services.RoonApiTransport.control(zone, "next");
-      await server.services.RoonApiTransport.control(zone, "stop");
-    }
+    await server.services.RoonApiTransport.control(zone, "stop");
+    await server.services.RoonApiTransport.control(zone, "next");
+    await server.services.RoonApiTransport.control(zone, "stop");
   }
   await standbyPromise(command.type, zone.outputs, server.services.RoonApiTransport);
 };
