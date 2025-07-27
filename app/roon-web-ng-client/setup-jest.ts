4b7edc743856214ebc6f1e8e76cc05157929a487
nihil <nihil@nihilux.org>
2025-07-27 15:40:25 -0400

    chore: switch to zoneless, zone is still used under the hood by jest-preset-angular (#614)

--- app/roon-web-ng-client/setup-jest.ts
+++ app/roon-web-ng-client/setup-jest.ts
@@ -5,7 +5,7 @@ import { nanoidMock } from "@mock/nanoid.mock";
 ngMocks.autoSpy('jest');
 
 import { CommonModule } from '@angular/common'; // eslint-disable-line import/order
-import { ApplicationModule } from '@angular/core'; // eslint-disable-line import/order
+import { ApplicationModule, provideZonelessChangeDetection } from "@angular/core"; // eslint-disable-line import/order
 import { BrowserModule } from '@angular/platform-browser'; // eslint-disable-line import/order
 import { mockRoonWorker } from "@mock/worker.utils.mock";
 import { TestBed } from "@angular/core/testing";
@@ -16,6 +16,7 @@ ngMocks.globalKeep(CommonModule, true);
 ngMocks.globalKeep(BrowserModule, true);
 TestBed.configureTestingModule({
   imports: [ApplicationModule, CommonModule, BrowserModule, NoopAnimationsModule],
+  providers: [provideZonelessChangeDetection()]
 });
 
 mockRoonWorker();
