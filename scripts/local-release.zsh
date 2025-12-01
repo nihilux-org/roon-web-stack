f1157dd1c03f13d2c91f09202fa447f2bab9764f
nihil <nihil@nihilux.org>
2025-12-01 09:06:52 -0500

    chore: clean ci/cd

--- scripts/local-release.zsh
+++ scripts/local-release.zsh
@@ -52,8 +52,7 @@ script_source=$(readlink -f "${0%/*}")
 cd "${script_source}/.." || exit;
 
 printf "%s%-55s%s\n" "${fg_bold[green]}" "building the js bundles:" "${reset_color}";
-yarn;
-yarn build;
+yarn cd;
 printf "%s%-55s%s" "${fg_bold[green]}" "js bundles built: " "${reset_color}";
 printf "%s\n" "📦 => ✅ ";
 
