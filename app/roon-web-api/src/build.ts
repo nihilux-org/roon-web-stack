36254c8741375f847080be57e827bfb214de9f50
nihil <nihil@nihilux.org>
2026-02-02 22:19:51 -0500

    chore: clean build

---
+++ app/roon-web-api/src/build.ts
@@ -0,0 +1,129 @@
+#!/usr/bin/env bun
+import { $, BuildConfig } from "bun";
+import { dirname } from "path";
+import { chdir } from "process";
+
+const EXTERNALS = [
+  "node-roon-api-audioinput",
+  "node-roon-api-browse",
+  "node-roon-api-image",
+  "node-roon-api-settings",
+  "node-roon-api-status",
+  "node-roon-api-transport",
+];
+
+const outdir = "bin";
+
+const log = (message: string, level = "info", arg?: unknown) => {
+  const toLog: unknown[] = [message];
+  if (arg !== undefined) {
+    toLog.push(arg);
+  }
+  switch (level) {
+    case "error":
+      // eslint-disable-next-line no-console
+      console.error(...toLog);
+      break;
+    case "info":
+      // eslint-disable-next-line no-console
+      console.log(...toLog);
+      break;
+    default:
+      // eslint-disable-next-line no-console
+      console.debug(...toLog);
+      break;
+  }
+};
+
+const logError = (message: string, arg?: unknown) => {
+  log(message, "error", arg);
+};
+
+const clean = async (baseDirectory: string) => {
+  try {
+    await $`rm -rf ${baseDirectory}/${outdir}`;
+    log("cleaned");
+  } catch (e) {
+    if (e instanceof $.ShellError) {
+      logError(e.text());
+      process.exit(e.exitCode);
+    }
+  }
+};
+
+const cdBin = async (baseDirectory: string) => {
+  const binDir = `${baseDirectory}/${outdir}`;
+  await $`mkdir ${binDir}`;
+  chdir(binDir);
+};
+
+const doBuild = async (target: string, baseDirectory: string, config: Partial<BuildConfig>) => {
+  const buildConfig = {
+    entrypoints: [`${baseDirectory}/src/app.ts`],
+    minify: true,
+    sourcemap: true,
+    external: EXTERNALS,
+    ...config,
+  };
+  const build = await Bun.build(buildConfig);
+  if (!build.success) {
+    logError("build failed");
+    for (const log of build.logs) {
+      logError(log.message);
+    }
+    process.exit(1);
+  }
+
+  log(`build ${target} done, ${build.outputs.length} file(s) generated:`);
+  for (const file of build.outputs) {
+    log(`file ${file.path}: ${Math.ceil(file.size / 1024)} KB`);
+  }
+
+  await $`mkdir ${baseDirectory}/${outdir}/node_modules`;
+  for (const external of EXTERNALS) {
+    await $`cp -rL ${baseDirectory}/node_modules/${external} ${baseDirectory}/${outdir}/node_modules/`;
+    log(`with external lib ${baseDirectory}/${outdir}/node_modules/${external}`);
+  }
+};
+
+const main = async (target: string) => {
+  try {
+    const roonWebApiBaseDirectory = dirname(dirname(import.meta.url.replace("file://", "")));
+    const outdirFullPath = `${roonWebApiBaseDirectory}/${outdir}`;
+    switch (target) {
+      case "clean":
+        await clean(roonWebApiBaseDirectory);
+        break;
+      case "binary":
+        await clean(roonWebApiBaseDirectory);
+        await cdBin(roonWebApiBaseDirectory);
+        await doBuild(target, roonWebApiBaseDirectory, {
+          compile: {
+            outfile: `${outdirFullPath}/roon-web-api`,
+            autoloadPackageJson: true,
+          },
+        });
+        break;
+      case "debug":
+        await clean(roonWebApiBaseDirectory);
+        await cdBin(roonWebApiBaseDirectory);
+        await doBuild(target, roonWebApiBaseDirectory, {
+          outdir: outdirFullPath,
+          target: "bun",
+        });
+        break;
+      default:
+        log(`Usage: bun run build.ts <target>
+Targets:
+  build       Production build (single executable)
+  build:debug Debug build (bin/ with sourcemaps)
+  clean       Remove bin/ directory`);
+        process.exit(0);
+    }
+  } catch (err) {
+    logError("oups...🤷...", err);
+    process.exit(1);
+  }
+};
+
+void main(process.argv[2] ?? "help");
--- scripts/local-web-abi.zsh
+++ scripts/local-web-abi.zsh
@@ -18,17 +18,13 @@ if (( $#flag_help )); then
 fi
 
 script_source=$(readlink -f "${0%/*}")
-cd "${script_source}/.." || exit;
 
-ROON_WEB_API_MODULE_PATH="./app/roon-web-api"
-ROON_WEB_API_BUILD_PATH="${ROON_WEB_API_MODULE_PATH}/bin"
-ROON_WEB_API_BUILD_NODE_MODULE_PATH="${ROON_WEB_API_BUILD_PATH}/node_modules"
-ROON_WEB_STACK_ROOT_NODE_MODULES="./node_modules"
-ROON_WEB_STACK_ROOT_NODE_MODULES_BUN="${ROON_WEB_STACK_ROOT_NODE_MODULES}/.bun"
-ROON_WEB_STACK_NG_CLIENT_BUILD="./app/roon-web-ng-client/dist/roon-web-ng-client/browser"
-ROON_WEB_STACK_NG_CLIENT_BUILD_DESTINATION="${ROON_WEB_API_BUILD_PATH}/web"
+ROON_WEB_STACK_ROOT="${script_source}/.."
 
-rm -rf "${ROON_WEB_API_BUILD_PATH}"
+cd "${ROON_WEB_STACK_ROOT}" || exit;
+
+ROON_WEB_API_BIN="${ROON_WEB_STACK_ROOT}/app/roon-web-api/bin"
+ROON_WEB_NG_CLIENT_BUILD="${ROON_WEB_STACK_ROOT}/app/roon-web-ng-client/dist/roon-web-ng-client/browser"
 
 if (( $#flag_debug )); then
   bun run cd:debug;
@@ -36,25 +32,15 @@ else
   bun run cd;
 fi
 
-rm -f "${ROON_WEB_API_MODULE_PATH}"/.*.bun-build
+rm -f "${ROON_WEB_STACK_ROOT}/**/.*.bun-build"
 
-mkdir -p "${ROON_WEB_API_BUILD_NODE_MODULE_PATH}"
+cp -r "${ROON_WEB_NG_CLIENT_BUILD}" "${ROON_WEB_API_BIN}/web"
 
-#cp -R "${ROON_WEB_STACK_ROOT_NODE_MODULES_BUN}/node-roon-api@github+roonlabs+node-roon-api+5125839/node_modules/node-roon-api" "${ROON_WEB_API_BUILD_NODE_MODULE_PATH}/node-roon-api"
-cp -R "${ROON_WEB_STACK_ROOT_NODE_MODULES_BUN}/node-roon-api-audioinput@github+roonlabs+node-roon-api-audioinput+21ff59e/node_modules/node-roon-api-audioinput" "${ROON_WEB_API_BUILD_NODE_MODULE_PATH}/node-roon-api-audioinput"
-cp -R "${ROON_WEB_STACK_ROOT_NODE_MODULES_BUN}/node-roon-api-browse@github+roonlabs+node-roon-api-browse+98adcba/node_modules/node-roon-api-browse" "${ROON_WEB_API_BUILD_NODE_MODULE_PATH}/node-roon-api-browse"
-cp -R "${ROON_WEB_STACK_ROOT_NODE_MODULES_BUN}/node-roon-api-image@github+roonlabs+node-roon-api-image+a5f0efa/node_modules/node-roon-api-image" "${ROON_WEB_API_BUILD_NODE_MODULE_PATH}/node-roon-api-image"
-cp -R "${ROON_WEB_STACK_ROOT_NODE_MODULES_BUN}/node-roon-api-settings@github+roonlabs+node-roon-api-settings+67cd8ca/node_modules/node-roon-api-settings" "${ROON_WEB_API_BUILD_NODE_MODULE_PATH}/node-roon-api-settings"
-cp -R "${ROON_WEB_STACK_ROOT_NODE_MODULES_BUN}/node-roon-api-status@github+roonlabs+node-roon-api-status+504c918/node_modules/node-roon-api-status" "${ROON_WEB_API_BUILD_NODE_MODULE_PATH}/node-roon-api-status"
-cp -R "${ROON_WEB_STACK_ROOT_NODE_MODULES_BUN}/node-roon-api-transport@github+roonlabs+node-roon-api-transport+2ee6000/node_modules/node-roon-api-transport" "${ROON_WEB_API_BUILD_NODE_MODULE_PATH}/node-roon-api-transport"
-
-cp -R "${ROON_WEB_STACK_NG_CLIENT_BUILD}" "${ROON_WEB_STACK_NG_CLIENT_BUILD_DESTINATION}"
-
-cd "${ROON_WEB_API_BUILD_PATH}" || exit
+cd "${ROON_WEB_API_BIN}" || exit
 
 export PORT=8686
 export LOG_LEVEL=debug
-export WEB_NG_PATH=/Users/nihil/Github/roon-web-stack/app/roon-web-api/bin
+export WEB_NG_PATH="${ROON_WEB_API_BIN}"
 
 if (( $#flag_debug )); then
   bun run ./app.js
