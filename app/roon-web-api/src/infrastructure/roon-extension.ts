0bf24defd94004459a5805a6b0cc6e91cb2471ce
nihil <nihil@nihilux.org>
2026-07-13 18:29:28 -0400

    chore: bump beta version (#981)

--- app/roon-web-api/src/infrastructure/roon-extension.ts
+++ app/roon-web-api/src/infrastructure/roon-extension.ts
@@ -23,7 +23,7 @@ import {
 import { Extension } from "@roon-kit";
 import { settingsOptions } from "./roon-extension-settings";
 
-export const extension_version = "0.0.14-beta-2";
+export const extension_version = "0.0.14-beta-3";
 
 const extension: RoonExtension<ExtensionSettings> = new Extension({
   description: {
